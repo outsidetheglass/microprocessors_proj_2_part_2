bc40143b4a2610cba63c1c0f3251de0243b9bed3
Stevie <outsidetheglass93@gmail.com>
2019-12-04 19:16:05 -0800

    Add files via upload
    Updated to be actual initialization. Still need to create a stack and the int director and assorted procedures to use a button. Also has my modified startup file with the IRQ added to interrupt at the right place.

--- button.s
+++ button.s
@@ -16,23 +16,41 @@ _start:
 @ Load in values for LEDs high
 	MOV R7,#0x01200000 		@ value for turning on high LEDs 3 and 0
 	MOV R8,#0x00C00000		@ value for turning on high LEDs 2 and 1
-	MOV R11,#0x04000000		@ value for initializing the button
 	
-	ADD R5,R0,#0x148		@ address for GPIO1_FALLINGDETECT
 	ADD R4,R0,#0x194		@ address for GPIO1_SETDATAOUT
 	ADD R3,R0,#0x190		@ address for GPIO1_CLEARDATAOUT
 	
-	STR R11,[R5]			@ set falling detect for button
 	STR R7,[R4]				@ put LED 3 and 0 high value into the set register to turn on
 	STR R8,[R3]				@ put LED 2 and 1 high value in to clear them
 	
 @ Now lets actually initialize the output
+	ADD R5,R0,#0x148		@ address for GPIO1_FALLINGDETECT
+	LDR R12,[R5]			@ Read current falling detect register
+	MOV R11,#0x04000000		@ value for initializing the button
+	ORR R12,R12,R11			@ Set bit 26
+	STR R12,[R5]			@ set falling detect for button
+	ADD R5,R0,#0x34			@ Address for IRQ status register
+	STR R11,[R5]			@ enable pointer pend for GPIO1_26 pin
+	
 	ADD R1,R0,#0x134		@ Register address for GPIO1_OE
 	LDR R6,[R1]				@ Read current OE register
 	MOV R9,#0xFE1FFFFF		@ enable GPIO1_21 - GPIO1_24
 	AND R6,R9,R6			@ Modify word to put back in
 	STR R6,[R1]				@ put back in to OE register
-	B BLINK
+	
+@ initialize the INTC
+	LDR R1,=0x482000E8		@ address of INTC_MIR_CLEAR3
+	MOV R2,#0x04			@ value for unmasking
+	STR R2,[R1]				@ write to the register
+@ enable processor IRQ in CPSR
+	MRS R3, CPSR			@ Copy CPSR to R3
+	BIC R3,#0x80			@ Clear bit 7
+	MSR CPSR_c, R3			@ write back to CPSR
+@ Wait for Interrupt
+LOOP:	NOP
+		B LOOP
+INT_DIRECTOR:
+@ put code here
 @ blink the USR LEDs in the correct order
 BLINK:
 	STR R7,[R4]				@ put LED 3 and 0 high value into the SETDATAOUT
