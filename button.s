cd4b010e18cdbde6d9b7768bbb7d001a30e7cafb
Stevie <outsidetheglass93@gmail.com>
2019-12-10 13:29:41 -0800

    Add files via upload
    Okay NOW it actually works as the project is supposed to. Turns out the LEDs are supposed to turn off with button press and with the next button press be on and blinking.

--- button.s
+++ button.s
@@ -1,130 +1,152 @@
-@ Code to make the user LEDs of a ARM processor blink
-@ first to turn on LEDs 3 and 0
-@ if button is pushed
-@ then turn on LEDs 2 and 1
-@ if button is pushed turn on LEDs 3 and 0 
-@ and repeat that endlessly
-@ written by Stephanie "Stevie" Taylor, Dec 2019
-
 .text
 .global _start
 .global INT_DIRECTOR
 _start:
-	MOV R10,#0				@ counter for the delay loop
-@ Stack stuff
-	LDR R13,=STACKSVC		@ point to base of stack for SVC mode
-	ADD R13,R13,#0x1000		@ point to top of stack
-	CPS #0x12				@ Switch to IRQ mode
-	LDR R13,=STACKIRQ		@ point to IRQ stack
-	ADD R13,R13,#0x1000		@ now to the top of the IRQ stack
-	CPS #0X13				@ Back to SVC mode
-@ clock stuff
-	LDR R0,=#0x02 			@ value to write to the clock register
-	LDR R1,=0x44E000AC		@ CM_PER_CM_PER_GPIO1_CLKCTRL
-	STR R0,[R1] 			@ write to register
-	LDR R0,=0x4804C000 		@ lets do this the fun way - starting at the base address for GPIO1 registers
-@ Load in values for LEDs high
-	MOV R7, #0x00C00000		@ load value for turning LEDs 2 and 1 on first
-	@ #0x01200000 is value for turning on high LEDs 3 and 0
-	@ #0x00C00000 is value for turning on high LEDs 2 and 1
+@ stack logic
+ 	LDR R13, =STACK1		@ point to base of stack1
+	ADD R13, R13, #0x1000
+	CPS #0x12
+	LDR R13, =STACK2		@ point to base of stack2
+	ADD R13, R13, #0x1000
+	CPS #0x13
+@ turn on GPIO1 clock
+	LDR R0, =#0x02
+	LDR R1, =0x44E000AC
+	STR R0, [R1]
+@ GPIO registers and their logic
+	LDR R11, =0x4804C000 	@ base address for GPIO1
+	MOV R4, #0x00C00000		@ high value for LEDs 2 and 1
+	STR R4, [R11, #0x190]	@ store LEDs 2 and 1 high value into EA = base address + CLEARDATAOUT offset 
+	ADD R5, R4, #0x00600000	@ value for increasing R4 to LEDs 3 and 0 high
+	STR R5, [R11, #0x194]	@ store LEDs 3 and 0 high value into base address + SETDATAOUT offset 
+@ GPIO1_OE logic for USR LEDS 0 through 3
+	LDR R6, =0xFE1FFFFF
+	STR R6, [R11, #0x134]
+@ set up GPIO_FALLINGEDGE
+	MOV R2,	#0x20000000
+	STR R2, [R11, #0x14C]	@ store high for pin 29 into EA = base address + FALLINGEDGE offset
 	
-	ADD R10,R0,#0x194		@ address for GPIO1_SETDATAOUT
-	ADD R11,R0,#0x190		@ address for GPIO1_CLEARDATAOUT
+@ use IRQ, POINTER PEND 1
+	STR R2,[R11, #0x34]			@ enable pointer pend for GPIO1_29 pin
+@ initialize INTC
+	LDR R1, =0x48200010		@ new base address, for INTC_CONFIG
+	MOV R3, #0x2			@ reset INTC_CONFIG register
+	STR R3, [R1]			@ write the value in to EA = base address
+	MOV R2, #0x04			@ unmasking value
+	STR R2, [R1, #0xD8]		@ write unmasking value in to EA of offset for MIR_CLEAR3 = INTC_CONFIG + D8
 	
-	STR R7,[R10]				@ put LED 1 and 2 high value into the set register to turn on
-	ADD R7,R7,#0x00600000	@ add to get value for setting LEDs 3 and 0
-	STR R7,[R11]				@ put LED 3 and 0 high value in to clear them
-	@ at this point R7 holds the value for LED 3 and 0 high
+@ CPSR enable for IRQ
+	MRS R3, CPSR			@ copy CPSR to R3
+	BIC R3, #0x80			@ clear bit 7
+	MSR CPSR_c, R3			@ write to CPSR	
+	
+@ flag bit for if LEDs blinking are on or not
+	MOV R10, #0x01			@ 1 is on, 0 is off
 
-	ADD R1,R0,#0x134		@ Register address for GPIO1_OE
-	LDR R6,[R1]				@ Read current OE register
-	MOV R9,#0xFE1FFFFF		@ enable GPIO1_21 - GPIO1_24
-	AND R6,R9,R6			@ Modify word to put back in
-	STR R6,[R1]				@ put back in to OE register
+
+@ From here, loop blinking LEDs 1 and 2 and then LEDS 3 and 0, and so on
+
+@ this first tests if the flag bit is set
+
+@ if flag is set, then go into BLINK_30 and blink LEDs 3 and 0
+@ then delay 2 seconds with DELAY1
+@ then go to BLINK_21 and blink LEDs 2 and 1
+@ then use DELAY2 to delay another 2 seconds, then go back to BLINK_30
+@ repeat this until interrupted
+
+@ if flag is not set, then go to LEDS_OFF
+@ run a delay in DELAY3 to keep them off while waiting for an interrupt
+
+LOOP:
+	NOP
+	TST R10, #0x01			@ test if LEDs blinking is set right now
+	BEQ LEDS_OFF			@ if they are blinking, turn them off
+	BNE BLINK_21			@ if they aren't, pass this and go into Blinking
+BLINK_21:
+	STR R5, [R11, #0x190]	@ first set LEDs 3 and 0 to off
+	STR R4, [R11, #0x194]	@ put LEDs 2 and 1 high value into the SETDATAOUT
+	MOV R6, #0x00400000		@ delay  
+	B DELAY1				@ then go delay 
+DELAY1:
+	TST R10, #0x01			@ test if LEDs blinking is set right now
+	BEQ LEDS_OFF			@ if they are blinking, turn them off
+	SUBS R6,#1				@ countdown
+	BNE DELAY1
+	B BLINK_30
+BLINK_30:
+	STR R4, [R11, #0x190]	@ first set LEDs 2 and 1 to off
+	STR R5, [R11, #0x194]	@ put LEDs 3 and 0 high value into the SETDATAOUT
+	MOV R6, #0x00400000		@ delay 
+	B DELAY2				@ then go delay
+DELAY2:
+	SUBS R6,#1				@ countdown
+	BNE DELAY2
+	B LOOP	
 	
-@ Now lets actually initialize the output
-	ADD R1,R0,#0x14C		@ address for GPIO1_FALLINGDETECT
-	LDR R2,[R1]			@ Read current falling detect register
-	MOV R2,#0x20000000		@ value for initializing the button
-	ORR R3,R3,R2			@ Set bit 26
-	STR R3,[R1]			@ set falling detect for button
-	ADD R1,R0,#0x2C			@ Address for IRQ status register
-	STR R2,[R1]			@ enable pointer pend for GPIO1_26 pin
-	
-@ initialize the INTC
-	LDR R1,=0x482000E8		@ address of INTC_MIR_CLEAR3
-	MOV R2,#0x04			@ value for unmasking
-	STR R2,[R1]				@ write to the register
-	
-@ enable processor IRQ in CPSR
-	MRS R3, CPSR			@ Copy CPSR to R12
-	BIC R3,#0x80			@ Clear bit 7
-	MSR CPSR_c, R3			@ write back to CPSR
-	
-@ Wait for Interrupt
-LOOP:	NOP
-		B LOOP
+LEDS_OFF:
+	MOV R9, #0x01E00000		@ all USR LEDS high value 
+	STR R9, [R11, #0x190]	@ put LEDs high value into the CLEARDATAOUT
+	MOV R6, #0x00400000		@ delay
+	B DELAY3
+DELAY3:
+	SUBS R6,#1				@ countdown
+	BNE DELAY3
+	MOV R6, #0x00000400
+	TST R10, #0x01			@ test if LEDs blinking is set right now
+	BNE BLINK_21			@ if they aren't, pass this and go into Blinking
+	B DELAY3	
+	B LOOP	
+
+@ IRQ for the button
 INT_DIRECTOR:
-	STMFD SP!, {R0-R3, LR}	@ push registers to my stack
-	LDR R0,=0x482000F8		@ address of INTC-PENDING_IRQ3 register
-	LDR R1,[R0]				@ read the current value of INTC-PENDING_IRQ3
-	TST R1,#0x00000004		@ test bit 2
-	BEQ PASS_ON				@ Not from GPIO INT1A so go back to the pass on loop
-	LDR R0,=0x04000000		@ else it is from GPIOINT1A, check if bit 26 is 1 
-	BNE BUTTON_SVC			@ if it is 1, then button pushed and go to button svc loop
-	BEQ PASS_ON				@ if it is not 1, then go back to wait loop
+	STMFD SP!, {R0-R3, LR}	@ push registers on stack
+CHECK_INTC:
+@ base address for the INTC
+	LDR R0,=0x48200000
+	LDR R2, [R0, #0xF8]		@ read into R2 from the EA of base address + INTC PENDING IRQ3 register offset		
+	TST R2, #0x4				@ test R2 against unmask
+	BNE CHECK_GPIO			@ go to check button if so
+	BEQ	PASS_ON				@ else go to pass on
+CHECK_GPIO:	
+	LDR R2, [R11, #0x2C]	@ check IRQ
+	TST R2, #0x20000000		@ test it is equal
+	BNE BUTTON_SVC			@ if so, go to button service
+	B PASS_ON				@ else go to pass on
 PASS_ON:
-	LDMFD SP!, {R0-R3,LR}	@ restore registers
-	SUBS PC, LR, #4			@ pass execution to the wait loop
+	LDMFD SP!, {R0-R3, LR}	@ restore registers
+	SUBS PC, LR, #4			@ Pass execution to blinking loop	
 BUTTON_SVC:
-	MOV R1,#0x04000000		@ value turns off GPIO1_26 and INTC interrupt requests - turning our "phone ringer" off
-	STR R1,[R0]				@ write to GPIO1_IRQSTATUS0 register
-	
-@ handle responding to new IRQ
-	LDR R0,=0x48200048		@ address of INTC_CONTROL register
-	MOV R1, #01				@ Value to clear bit 0
-	STR R1,[R0]				@ Write to INTC_CONTROL register
-	
-@ toggle logic
-	TST R7,#0x01200000		@ if current R7 address means LEDs 3 and 0 are on 
-	BNE BLINK12				@ then branch into the procedure for switching to LEDs 2 and 1 on
-	BEQ BLINK03				@ else turn on BLINK03
+	@ turn off IRQ request
+	MOV R2, #0x20000000		@ turn off ringer value
+	STR R2, [R11, #0x2C]	@ ringer off value put into GPIO1_IRQSTATUS
+	MOV R1, #0x01			@ to clear bit 0
+	STR R1, [R0, #0x48]		@ ringer off value write to INTC_control
+	TST R10, #0x01			@ test if LEDs blinking is set right now
+	BNE TURN_OFF			@ if so, turn blinking LEDs off
+	BEQ TURN_ON				@ and go to pass on
+TURN_OFF:
+	MOV R10, #0x00			@ set flag bit to show LEDs are off
+	B PASS_ON
+TURN_ON:
+	MOV R10, #0x01			@ set flag bit to show LEDs are off
+	B PASS_ON @ LIGHT instead of PASSON
+@ DELAY loop
+	@ Check IRQ status_0
+	@ read it
+	@ test for button push
+	@ BNE go to button SVC
+	@ BEQ goes to outside lights (3 and 0)
+	@ else go to inside lights (2 and 1)
+	@ 
 
-@ if the code gets here, that means R7 is neither representing LEDs 3 and 0 or LEDs 2 and 1 being on
-@ so lets turn one set on here, just to be safe
-	MOV R7, #0x00C00000		@ load value for turning LEDs 2 and 1 on first
-	B BLINK03				@ then branch into the procedure for switching to LEDs 3 and 0 on
 
-@ blink the USR LEDs 3 and 0
-BLINK03:
-	STR R7,[R11]				@ put LED 2 and 1 high value into the CLEARDATAOUT
-	ADD R7,R7,#0x00600000	@ add to get value for setting LEDs 3 and 0
-	STR R7,[R10]				@ put LED 3 and 0 high value into the SETDATAOUT		
-	MOV R10,#0x00400000		@ delay value
-	B DELAY
-	
-@ blink the USR LEDs 2 and 1
-BLINK12:
-	STR R7,[R11]				@ put LED 3 and 0 high value into the CLEARDATAOUT
-	SUB R7,R7,#0x00600000	@ subtract to get value for setting LEDs 1 and 2	
-	STR R7,[R10]				@ put LED 2 and 1 high value into the SETDATAOUT
-	MOV R10,#0x00400000		@ delay value
-	B DELAY
-@ loop for 2 seconds
-DELAY:
-	SUBS R10,#1				@ countdown
-	BNE DELAY
-	@ branch to wait for interrupt loop
-	LDMFD SP!, {R0-R3,LR}	@ restore registers
-	SUBS PC, LR, #4			@ return from IRQ interrupt procedure
-.align 2
-SYS_IRQ:	.WORD 0			@ location for system's IRQ address
+
 .data
 .align 2
-STACKSVC:	.rept 1024
-			.word 0x0000
-			.endr
-STACKIRQ:	.rept 1024
-			.word 0x0000
-			.endr
-.END 
+STACK1:	.rept 1024
+	.word 0x0000
+	.endr
+STACK2:	.rept 1024
+	.word 0x0000
+	.endr
+.END
